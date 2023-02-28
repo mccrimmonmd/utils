cc97b8dfcb00019d6d60c8d04e9bcbe77a03eb1e
mccrimmonmd <saturnhs@gmail.com>
2023-02-28 10:29:11 -0700

    Added `randDigit` and `randDigitString`

--- random.js
+++ random.js
@@ -1,11 +1,14 @@
-randNum = (lessThan) => Math.random() * lessThan
-randInt = (lessThan) => Math.floor(randNum(lessThan))
-randChoice = (arr) => arr[randInt(arr.length)]
-coinFlip = () => Math.random() < 0.5
-
 module.exports = {
-  randNum,
-  randInt,
-  randChoice,
-  coinFlip,
+  randNum: (lessThan) => Math.random() * lessThan,
+  randInt: (lessThan) => Math.floor(randNum(lessThan)),
+  randDigit: () => randInt(10),
+  randDigitString: (length) => {
+    let digits = []
+    for (let i = 0; i < length; i++) {
+      digits.push(randDigit())
+    }
+    return digits.join('')
+  },
+  randChoice: (arr) => arr[randInt(arr.length)],
+  coinFlip: () => Math.random() < 0.5,
 }
