43a2751f72b7ac8749778c71e344d5f395331ae5
saturnhs@gmail.com <saturnhs@gmail.com>
2023-04-26 03:32:10 -0600

    Added 'shuffled' method to random.js

--- random.js
+++ random.js
@@ -3,12 +3,21 @@ module.exports = {
   randInt: (lessThan) => Math.floor(randNum(lessThan)),
   randDigit: () => randInt(10),
   randDigitString: (length) => {
-    let digits = []
-    for (let i = 0; i < length; i++) {
-      digits[i] = randDigit()
-    }
-    return digits.join('')
+    return new Array(length).map(_ => randDigit()).join('')
+    // let digits = []
+    // for (let i = 0; i < length; i++) {
+    //   digits[i] = randDigit()
+    // }
+    // return digits.join('')
   },
   randChoice: (arr) => arr[randInt(arr.length)],
+  shuffled: (arr) => {
+    arr = arr.slice()
+    for (let i = arr.length; i > 1; i--) {
+      let place = randInt(i)
+      [arr[place], arr[i - 1]] = [arr[i - 1], arr[place]]
+    }
+    return arr
+  },
   coinFlip: () => Math.random() < 0.5,
 }
