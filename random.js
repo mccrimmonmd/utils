5fc2e904b63a8218c100fb423ad34b61ec310b04
mccrimmonmd <saturnhs@gmail.com>
2023-04-28 10:52:08 -0600

    Tweaks and bugfixes

--- random.js
+++ random.js
@@ -1,23 +1,26 @@
+const randNum = (lessThan) => Math.random() * lessThan
+const randInt = (lessThan) => Math.floor(randNum(lessThan))
+const randDigit = () => randInt(10)
+const randDigitString = (length) => 
+  new Array(length).fill(0).map(_ => randDigit()).join('')
+const randChoice = (arr) => arr[randInt(arr.length)]
+const shuffle = (arr) => {
+  for (let i = arr.length; i > 1; i--) {
+    let place = randInt(i)
+    let tmp = arr[place]
+    arr[place] = arr[i - 1]
+    arr[i - 1] = tmp
+  }
+  return arr
+}
+const coinFlip = () => Math.random() < 0.5
+
 module.exports = {
-  randNum: (lessThan) => Math.random() * lessThan,
-  randInt: (lessThan) => Math.floor(randNum(lessThan)),
-  randDigit: () => randInt(10),
-  randDigitString: (length) => {
-    return new Array(length).map(_ => randDigit()).join('')
-    // let digits = []
-    // for (let i = 0; i < length; i++) {
-    //   digits[i] = randDigit()
-    // }
-    // return digits.join('')
-  },
-  randChoice: (arr) => arr[randInt(arr.length)],
-  shuffled: (arr) => {
-    arr = arr.slice()
-    for (let i = arr.length; i > 1; i--) {
-      let place = randInt(i)
-      [arr[place], arr[i - 1]] = [arr[i - 1], arr[place]]
-    }
-    return arr
-  },
-  coinFlip: () => Math.random() < 0.5,
+  randNum,
+  randInt,
+  randDigit,
+  randDigitString,
+  randChoice,
+  shuffle,
+  coinFlip,
 }
