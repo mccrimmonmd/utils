e38e833d961ec29f316aa91ef6c04f9d468e4304
mccrimmonmd <saturnhs@gmail.com>
2023-11-07 16:29:37 -0700

    Added some functions for drawing ASCII art
    Will organize properly later

---
+++ draw.js
@@ -0,0 +1,41 @@
+const arrayFrom = (length, item) => Array.from({ length }, () => item)
+const stringFrom = (length, character) => arrayFrom(length, character).join('')
+
+const drawRow = (pictures, padding = 0) => {
+  let lines = combinePics(pictures, padding)
+  console.log(lines.join('\n')
+  return lines
+}
+
+const combinePics = (pictures, padding = 0) => {
+  pictures = pictures.map(makeRectangular)
+  let lines = []
+  let width = pictures.reduce((lineWidth, pic) => lineWidth + pic[0].length, 0)
+  let height = pictures.reduce((maxHeight, pic) => {
+    return maxHeight < pic.length ? pic.length : maxHeight
+  }, 0)
+  for (let i = 0; i < height; i++) {
+    let line = []
+    for (let j = 0; j < pictures.length; j++) {
+      let pic = pictures[j]
+      let str = pic.length < i + 1 ? stringFrom(pic[0].length, ' ') : pic[i]
+      line.push(str)
+      if (padding) line.push(stringFrom(padding, ' '))
+    }
+    lines.push(line.join(''))
+  }
+  return lines
+}
+
+const makeRectangular = (pic) => {
+  if (!Array.isArray(pic)) pic = pic.split('\n')
+  if (pic == null || pic.length === 0) {
+    return ['']
+  }
+  pic = pic.map(line => line == null ? '' : line)
+  let maxLength = pic.reduce(
+    (len, line) => len < line.length ? line.length : len,
+    0
+  )
+  return pic.map(line => line + stringFrom(maxLength - line.length, ' '))
+}
