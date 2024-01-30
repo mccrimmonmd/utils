eeafa91cb1597d32ae0e9b07b45a3882b097e62d
mccrimmonmd <saturnhs@gmail.com>
2024-01-30 11:44:51 -0700

    Typo in 'isEmpty'

--- utils/general.js
+++ utils/general.js
@@ -25,7 +25,7 @@ const isEmpty = (value, alwaysEmpty=[], neverEmpty=[]) => {
   if (value == null) return true
   if (typeof value === 'boolean') return false
   if (value.length != null) return value.length === 0
-  if (value.size != null) return value.length === 0
+  if (value.size != null) return value.size === 0
   if (typeof value === 'object' && Object.keys(value).length === 0) return true
   // ^ Gives false positive on Sets, Maps, etc.
   // (covered by length & size checks, but are there others?)
