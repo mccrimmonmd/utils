867c73a9cb91e5cf3f55c856476bfbe16d317523
Malcolm McCrimmon <malcolm.mccrimmon@dish.com>
2024-08-29 12:22:44 -0600

    Renamed 'removeRandChoice' to 'randRemove'
    I think it better complements 'randChoice' this way, and it's shorter, too!

--- utils/random.js
+++ utils/random.js
@@ -35,8 +35,8 @@ const randDigitString = (length) => {
 myself.randChoice = "Returns a random element from the given array."
 const randChoice = (arr) => arr[randInt(arr.length)]
 
-myself.removeRandChoice = "Removes (in-place) a random element from the given array. Returns the removed element."
-const removeRandChoice = (arr) => arr.splice(randInt(arr.length), 1)
+myself.randRemove = "Removes (in-place) a random element from the given array. Returns the removed element."
+const randRemove = (arr) => arr.splice(randInt(arr.length), 1)
 
 myself.shuffled = "Returns a shuffled copy of the given array."
 const shuffled = (arr) => {
@@ -70,6 +70,6 @@ module.exports = {
   randDigit,
   randDigitString,
   randChoice,
-  removeRandChoice,
+  randRemove,
   shuffled,
 }
