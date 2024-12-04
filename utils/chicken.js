07c27d6f08336956be006d9f2079727484748f79
mccrimmonmd <saturnhs@gmail.com>
2024-12-04 12:52:48 -0700

    Moving logic for modifiers and multiples around
    Need to make sure everything's applied in the correct order...

--- utils/chicken.js
+++ utils/chicken.js
@@ -18,52 +18,58 @@ const multipleLevels = multipleNames.reduce((names, name, i) => {
   return names
 }, {})
 
-const play = (maxRounds = Infinity, totalScore = 0) => {
-  const stats = {}
-  const modifiers = arrayOf(5, 0)
+const applyModifiers = (rolls, modifiers) => {
+  const levels = rolls.map((roll, i) => roll + modifiers[i])
+  const [ tens, elevens, twelves ] = levels.reduce((highs, level) => {
+    if (level >= 10) highs[0] += 1
+    if (level === 11) highs[1] += 1
+    if (level >= 12) highs[2] += 1
+    return highs
+  }, [0, 0, 0])
+  if (tens > 0) {
+    // update stats
+    if (firstTen) {
+      console.log('First ten!')
+      // update stats
+      firstTen = false
+    }
+    // special rules
+    console.log(`Tens: ${tens}`)
+  }
+  if (twelves > 0) {
+    // update stats
+    if (firstTwelve) {
+      console.log('First twelve!')
+      // update stats
+      firstTwelve = false
+    }
+    console.log(`Twelves: ${twelves}`)
+  }
+  return [ levels, tens, elevens, twelves ]
+}
+
+const stats = {}
+const play = (maxRounds = Infinity, totalScore = 0, modifiers = arrayOf(5, 0)) => {
   let locked = arrayOf(5, false)
   let firstTen = true
   let firstTwelve = true
   let lockTwelves = false
+  let skipTests = 0 // 0 = none, 1 = quads, 2 = quints
+  let endgame = 0 // 0 = none, 1 = any tens, 2 = three tens, 3 = four tens & any twelves, 4 = all twelves
   console.log('START!')
   for (const round of range(maxRounds)) {
     // before the round
     let score = 1
     const rolls = dice.rollDice(5)
-    const levels = rolls.map((roll, i) => roll + modifiers[i])
     const multipleType = dice.getFullName(rolls)
-    const [ tens, elevens, twelves ] = levels.reduce((highs, level) => {
-      if (level >= 10) highs[0] += 1
-      if (level === 11) highs[1] += 1
-      if (level >= 12) highs[2] += 1
-      return highs
-    }, [0, 0, 0])
-    score += elevens
-    score += twelves * 2
-    if (tens > 0) {
-      // update stats
-      if (firstTen) {
-        console.log('First ten!')
-        // update stats
-        firstTen = false
-      }
-      // special rules
-      console.log(`Tens: ${tens}`)
-    }
-    if (twelves > 0) {
-      // update stats
-      if (firstTwelve) {
-        console.log('First twelve!')
-        // update stats
-        firstTwelve = false
-      }
-      if (lockTwelves) locked = levels. map(level => level >= 12)
-      console.log(`Twelves: ${twelves}`)
-    }
+    let [ levels, tens, elevens, twelves ] = applyModifiers(rolls, modifiers)
     // adjust multiple
     // apply multiple
     // apply z
     // other modifiers?
+    score += elevens
+    score += twelves * 2
+    if (lockTwelves) locked = levels. map(level => level >= 12)
     // during the round
     console.log(`Starting round ${round}...`)
     // end of the round
@@ -71,7 +77,7 @@ const play = (maxRounds = Infinity, totalScore = 0) => {
     console.dir(Object.fromEntries(zip(ruleNames, levels)))
     // TODO: calculate chance of losing (optional): return false
     totalScore += score
-    // TODO: simulate reset / end game decisions (or ask user): return totalScore / return play(maxRounds - round - 1[, totalScore])
+    // TODO: simulate reset / end game decisions (or ask user): return totalScore / return play(maxRounds - round - 1[, totalScore, modifiers])
     for (const [i, roll] of rolls.entries()) {
       if (roll === 6) modifiers[i] += 1
     }
