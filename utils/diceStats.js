4311c2c0881f53902e31ee6fd01d53de9928effa
Malcolm Maes-McCrimmon <malcolm.mccrimmon@dish.com>
2025-03-14 09:57:18 -0600

    Use arrayify in countSides for better type handling

--- utils/diceStats.js
+++ utils/diceStats.js
@@ -1,5 +1,5 @@
 const myself = {} // documentation
-const { print, arrayOf } = require('./general')
+const { print, arrayify, arrayOf } = require('./general')
 const { roundDecimal } = require('./numbers')
 const { rollDie, rollDice } = require('./random')
 const { allKeys } = require('./objects')
@@ -11,12 +11,13 @@ myself.countSides = "Generates (or takes as a parameter) a list of dice rolls an
 const countSides = (dice = 4, sides, rStyle = false) => {
   // 'sides' is optional, but it can't be a default parameter because the
   // default is different depending on whether 'dice' is a number or an array
-  if (Array.isArray(dice)) {
-    sides = sides ?? Math.max(...dice)
-  }
-  else {
+  if (['number', 'bigint'].includes(typeof dice)) {
     sides = sides ?? 6
     dice = rollDice(dice, sides)
+  }
+  else {
+    dice = arrayify(dice)
+    sides = sides ?? Math.max(...dice)
   }
   const multiples = arrayOf(sides, 0)
   dice = dice.toReversed()
