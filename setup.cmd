d139847a916bd84c15cd44b6451e05d2387d1620
Malcolm Maes-McCrimmon <saturnhs@gmail.com>
2025-05-19 22:52:05 -0600

    Update setup script to work in CMD (no ~ variable)

--- setup.cmd
+++ setup.cmd
@@ -7,16 +7,17 @@ REM Use cmd.exe instead of PowerShell to have apps added to Path automatically:
 winget install git.git
 winget install vim.vim
 winget install node.js
-winget install python.python
+winget install python.python.3.13
 winget install chocolatey
 REM ...
 
-cd ~\Documents\Git
+REM For PowerShell, use ~ instead--no variable that works in both :'(
+cd %USERPROFILE%\Documents\Git
 git clone https://github.com/mccrimmonmd/utils.git
 cd utils
-copy .gitconfig ~\
-copy _vimrc ~\
-REM copy <.ps config file> ~\
+copy .gitconfig %USERPROFILE%\
+copy _vimrc %USERPROFILE%\
+REM copy <.ps config file> %USERPROFILE%\
 
 REM clone any other repos?
 
