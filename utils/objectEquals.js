97f9c238aaf696fa72c3fe5c609d1d768f315b50
Malcolm Maes-McCrimmon <malcolm.mccrimmon@dish.com>
2025-09-08 12:25:26 -0600

    Fix whitespace

--- utils/objectEquals.js
+++ utils/objectEquals.js
@@ -9,9 +9,8 @@ module.exports = (
     maxDepth = Infinity,
   } = {}
 ) => {
-  if (objA === objB) {
-    return true
-  }
+  if (objA === objB) return true
+
   let typeOfA = typeof objA
   if (typeOfA !== typeof objB) {
     if (compareBigIntToNumber) {
@@ -22,11 +21,14 @@ module.exports = (
         return BigInt(objA) === objB
       }
     }
+
     return false
   }
+
   if (typeOfA === 'number' && isNaN(objA)) {
     return isNaN(objB)
   }
+
   if (typeOfA === 'function') {
     if (!compareFuncsWith) {
       throw new Error(
@@ -41,6 +43,7 @@ module.exports = (
       return true
     }
   }
+
   if (typeOfA === 'object') {
     if ( objA === null
       || maxDepth <= 0
@@ -97,5 +100,6 @@ module.exports = (
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol
     })
   }
+
   return false
 }
