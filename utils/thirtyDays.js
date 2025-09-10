969db593e6442aa4480e2f34c5843d112f1dc4cc
Malcolm Maes-McCrimmon <malcolm.mccrimmon@dish.com>
2025-09-09 18:56:35 -0600

    Use new arrayify to clean up default initialization in playRound

--- utils/thirtyDays.js
+++ utils/thirtyDays.js
@@ -1,4 +1,4 @@
-const { range, timeIt, findDupes } = require('./general')
+const { range, timeIt, findDupes, arrayify } = require('./general')
 const { roundDecimal, arithmeticMean } = require('./numbers')
 const { rollDice } = require('./random')
 const { sum } = require('./reducers')
@@ -65,13 +65,6 @@ const playRound = (
     perfectDays: 7,
     ...options
   }
-  const totals = [
-    0,
-    0,
-    0,
-    0,
-    0,
-  ]
   const names = [
     '',
     'Two Pair',
@@ -79,6 +72,7 @@ const playRound = (
     'Triple',
     'Perfect Roll'
   ]
+  const totals = arrayify(0, names.length)
   const findDupeTypes = (dupeSets) => {
     const dupeTypes = []
     for (const [i, dupes] of dupeSets.entries()) {
