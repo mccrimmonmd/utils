f57d659ea932e8964692c561c76c1febbf043fe1
Malcolm Maes-McCrimmon <malcolm.mccrimmon@dish.com>
2026-02-27 17:12:40 -0700

    Refactor en/decryption process into separate function

--- arcfour.js
+++ arcfour.js
@@ -30,19 +30,27 @@ const iVec = new Uint8Array(arrayify(() => randInt(256), 10))
 const ciphertext = '=SomE eXample(1)(!)[?]'
 const cipherbytes = stringToBytes(ciphertext)
 const keyText = 'asdfg'
+const crypt = (bytes, state) => {
+  const output = new Uint8Array(bytes.length)
+  let j = 0
+  for (const [k, cByte] of bytes.entries()) {
+    const i = k + 1
+    j += state[i]
+    swap(state, i, j)
+    let n = state[i] + state[j]
+    output[k] = state[n] ^ cByte
+  }
+  return output
+}
 const key = new Uint8Array([...stringToBytes(keyText), ...iVec])
 const state = indexWrapify([...range(256)])
 
 mix(key, iVec, state)
+const output = crypt(cipher, state)
 
-const crypt = new Uint8Array(cipherbytes.length)
-let j = 0
-for (const [k, cByte] of cipherbytes.entries()) {
-  const i = k + 1
-  j += state[i]
-  swap(state, i, j)
-  let n = state[i] + state[j]
-  crypt[k] = state[n] ^ cByte
-}
+console.log(bytesToString(
+  encoding ?
+    new Uint8Array([...iVec, ...output])
+  : output
+))
 
-console.log(bytesToString([...iVec, ...crypt]))
